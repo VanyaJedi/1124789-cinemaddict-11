83b776841f4bbe93cb1d80647447824f179ae45d
Ivan <vanya71310@gmail.com>
2020-04-16 20:34:26 +0300

    Добавляет выполненное задание

---
+++ src/components/nofilm.js
@@ -0,0 +1,29 @@
+
+import {createElement} from "../util.js";
+
+export default class NoFilm {
+
+  constructor(film) {
+    this._film = film;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return (
+      `<section class="films-list">
+        <h2 class="films-list__title">There are no movies in our database</h2>
+      </section>`
+    );
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
